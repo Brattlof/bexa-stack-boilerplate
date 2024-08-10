01ae5d875efa8cd7819c8b7a78cdedbd2d4bf026
Brattlof <brattlofcsgo@gmail.com>
2024-08-11 00:39:13 +0200

    Self host with bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 
+import bun from "@nurodev/astro-bun";
 import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel(), // bun()
   integrations: [tailwind()],
   build: {
     assets: "_",
